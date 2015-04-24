6a8833a821c48754c2e4ae6f3f35640b788c597e
pinak <pinak.0605@gmail.com>
2015-04-24 19:21:10 +0530

    sandbox braintree

--- vendor/gems/catarse_braintree-2c5b83730cf9/lib/catarse_braintree/gateway.rb
+++ vendor/gems/catarse_braintree-2c5b83730cf9/lib/catarse_braintree/gateway.rb
@@ -15,9 +15,9 @@ module CatarseBraintree
       check_default_configurations!
       Rails.logger.debug @configuration[:braintree_merchant_id]
       Braintree::Configuration.environment = !!@configuration[:braintree_test] ? :sandbox : :production
-      Braintree::Configuration.merchant_id = "5thmz3853jfktm9s"
-      Braintree::Configuration.public_key  = @configuration[:braintree_public_key]
-      Braintree::Configuration.private_key = @configuration[:braintree_private_key]
+      Braintree::Configuration.merchant_id = "jnp9mrw7jcfhdxd5"
+      Braintree::Configuration.public_key  = "dk4gvgnsp95yhjb2"
+      Braintree::Configuration.private_key = "b02e9a2884ced1ec00ec3a26cb749489"
       self
     end
 
