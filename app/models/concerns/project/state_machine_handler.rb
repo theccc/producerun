79ccf0426a9a8e845f08160ba915722644df9ee8
Brijesh <brijeshshah86@gmail.com>
2015-07-22 21:55:08 +0530

    Fixed user project state issue

--- app/models/concerns/project/state_machine_handler.rb
+++ app/models/concerns/project/state_machine_handler.rb
@@ -23,7 +23,7 @@ module Project::StateMachineHandler
       end
 
       event :send_to_analysis do
-        transition draft: :online
+        transition draft: :in_analysis
       end
 
       event :reject do
